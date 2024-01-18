800ee8be8ef1f8cb967d1b40a9593b90cf6e32b4
Cameron Olivier <cameron.olivier@mohara.co>
2024-01-18 16:58:27 +0200

    chore: it's nextjs now

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-/** @type {import("next").NextConfig} */
-const config = {
-    output:"standalone",
-};
-
-export default config;
+module.exports = nextConfig
