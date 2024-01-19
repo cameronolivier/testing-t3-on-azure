9991fb066490c963616dbc3d5be81aae8ab7d704
Cameron Olivier <cameron.olivier@mohara.co>
2024-01-19 10:55:10 +0200

    refactor: displaying next version in header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Package from '../package.json';
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! ({Package.dependencies.next})</a>
         </h1>
 
         <p className={styles.description}>
